127e0b3be7af530531a9f8643c16464bf247d235
kalaiarasi112007-cyber <kalaiarasi112007@gmail.com>
2026-04-15 19:47:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSection(id) {
+    let sections = document.querySelectorAll('.section');
+
+    sections.forEach(sec => {
+        sec.classList.remove('active');
+    });
+
+    document.getElementById(id).classList.add('active');
+}
